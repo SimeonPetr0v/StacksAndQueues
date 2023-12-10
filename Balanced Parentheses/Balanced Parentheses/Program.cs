51b230c448010949f0d005b6eca3222add28fa07
SimeonPetr0v <simeonp925@gmail.com>
2023-12-10 22:08:53 +0200

    Exercises

---
+++ Balanced Parentheses/Balanced Parentheses/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Balanced_Parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var stack = new Stack<char>();
+
+            bool balanced = true;
+
+            if (input.Length != 0)
+            {
+                foreach (var @char in input)
+                {
+                    switch (@char)
+                    {
+                        case '(':
+                        case '[':
+                        case '{':
+                            stack.Push(@char);
+                            break;
+                        case ')':
+                            if (!stack.Any() || stack.Pop() != '(')
+                            {
+                                balanced = false;
+                            }
+                            break;
+                        case ']':
+                            if (!stack.Any() || stack.Pop() != '[')
+                            {
+                                balanced = false;
+                            }
+                            break;
+                        case '}':
+                            if (!stack.Any() || stack.Pop() != '{')
+                            {
+                                balanced = false;
+                            }
+                            break;
+                    }
+                }
+            }
+
+            if (balanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
---
+++ Simple Text Editor/Simple Text Editor/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numberOfOperations = int.Parse(Console.ReadLine());
+
+            var textStack = new Stack<string>();
+            textStack.Push("");
+
+            for (int i = 0; i < numberOfOperations; i++)
+            {
+                var input = Console.ReadLine().Split();
+
+                var command = input[0];
+
+                switch (command)
+                {
+                    case "1":
+                        string currentText = textStack.Peek() + input[1];
+                        textStack.Push(currentText);
+                        break;
+                    case "2":
+                        string previousText = textStack.Peek();
+                        int elementsToRemove = int.Parse(input[1]);
+                        string newString = previousText.Substring(0, previousText.Length - elementsToRemove);
+                        textStack.Push(newString);
+                        break;
+                    case "3":
+                        string allTheText = textStack.Peek();
+                        int indexToReturn = int.Parse(input[1]);
+                        Console.WriteLine(allTheText[indexToReturn - 1]);
+                        break;
+                    case "4":
+                        textStack.Pop();
+                        break;
+                }
+            }
+        }
+    }
+}
